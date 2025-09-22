b8743289a496fd521e2899f8aef5db7ed01f3297
kim minji <gimminji1365@naver.com>
2025-09-22 16:42:05 +0900

    블로그 기본 기능 구현

---
+++ blog/src/main/java/com/leets/backend/blog/model/Post.java
@@ -0,0 +1,37 @@
+package com.leets.backend.blog.model;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+
+    public Post(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
